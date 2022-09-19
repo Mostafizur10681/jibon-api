9dea20df2ed24136de8c4fbba233b4238d39de68
Md. Riajul Islam <riaj17021995@gmail.com>
2022-09-19 20:42:45 +0600

    environment variable added

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 // import express
-const express = require('express');
+const express = require("express");
+require("dotenv").config();
+const userRoutes = require("./routes/userRoutes");
 
-const userRoutes = require('./routes/userRoutes')
-
-// import mongooes
-const mongoose = require('mongoose');
-
+// import mongoose
+const mongoose = require("mongoose");
 
 // port = 5000
 const port = process.env.PORT || 5000;
@@ -17,20 +16,18 @@ const app = express();
 app.use(express.json());
 
 // routes
-app.use('/user', userRoutes)
-
-
-
+app.use("/user", userRoutes);
 
 // connect to MongoDB
-mongoose.connect('mongodb+srv://jibonApi:n1XUgYkuYZf2Wtee@cluster0.0dldsqn.mongodb.net/?retryWrites=true&w=majority')
-    .then(() => {
-        app.listen(port, (req, res) => {
-            console.log('The Jibon app is running on port : ', port);
-        })
-    })
-    .catch((error) => {
-        console.log(error)
-    })
-
-
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0dldsqn.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(port, (req, res) => {
+      console.log("The Jibon app is running on port : ", port);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
