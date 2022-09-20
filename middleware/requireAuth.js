b719219778e77d2d7b9966b0423a7554ab8c80d3
Mostafizur rahman <sourav10681@gmail.com>
2022-09-20 16:32:49 +0600

    update requireAuth

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -1,56 +1,29 @@
-<<<<<<< HEAD
 
 const jwt = require("jsonwebtoken");
 const UserModel = require("../models/userModel")
-
-const requireAuth = async (req, res, next) => {
-
-
-    // verify user is authenticated
-    const { authorization } = req.headers;
-
-    if (!authorization) {
-        return res.status(401).send({ message: 'Unthorization access' })
-    }
-
-    const token = authorization.split(" ")[1];
-
-    try {
-        const { _id } = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-        req.user = await UserModel.findOne({ _id }).select("_id");
-        next();
-    } catch (error) {
-        console.log(error.message);
-        return res.status(403).send({ message: 'Forbidden Acess' })
-    }
-
-};
-
-module.exports = requireAuth;
-=======
-const UserModel = require("../models/userModel");
-const jwt = require("jsonwebtoken");
 require("dotenv").config();
-
 const requireAuth = async (req, res, next) => {
+
+
   // verify user is authenticated
   const { authorization } = req.headers;
 
   if (!authorization) {
-    return res.status(401).json({ error: "Unauthorized User" });
+    return res.status(401).send({ message: 'Unthorization access' })
   }
 
   const token = authorization.split(" ")[1];
 
   try {
-    const { email } = jwt.verify(token, "SECRET_KEY");
-
-    req.user = await UserModel.findOne({ email, role: "admin" });
+    const { _id } = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    req.user = await UserModel.findOne({ _id }).select("_id");
     next();
   } catch (error) {
-    res.status(401).json({ error: "Unauthorized User" });
+    console.log(error.message);
+    return res.status(403).send({ message: 'Forbidden Acess' })
   }
+
 };
 
 module.exports = requireAuth;
->>>>>>> bdbfa03c62fa8edf6185a104f856dabc5f7be59c
+
