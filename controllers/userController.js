357f2822066bd499ae88472acd055eb171213cb5
Mostafizur rahman <sourav10681@gmail.com>
2022-09-20 17:13:33 +0600

    updated some file

--- controllers/userController.js
+++ controllers/userController.js
@@ -45,19 +45,6 @@ const loginUser = async (req, res) => {
 
 };
 
-// update user Info
-// const updateUser = async (req, res) => {
-//     const { id } = req.params;
-
-//     try {
-//         const user = await UserModel.findOneAndUpdate({ _id: id }, { ...req.body })
-//         res.status(200).json(user);
-//     } catch (error) {
-//         res.status(400).json({ error: error.message });
-//     }
-// }
-
-
 const updateUser = async (req, res) => {
     const { id } = req.params;
     try {
